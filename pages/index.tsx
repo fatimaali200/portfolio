f4a9380e1e6ddf95addf99b54e8a26e584340262
Fatima Ali <fofoalmali@gmail.com>
2023-08-31 20:49:07 +0300

    feat: add navbar layot

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
+import Navbar from "@/components/Navbar";
+import Head from "next/head";
+
 export default function Home() {
   return (
-   <main className="font-bodyFont">Hello Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro mollitia dicta deleniti suscipit iste quos vel sunt aspernatur laborum voluptas? Inventore, esse labore amet quam quod illum in vel! Asperiores.</main>
-  )
+    <>
+    <Head>
+      <title>Fatima Ali</title>
+      <meta name="description" content=""/>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/logo.ico" />
+    </Head>
+    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-scroll"><Navbar /></main>
+    </>
+    )
 }
