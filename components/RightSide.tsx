99fc0f293653520c0eca474679a0f23ef7f590bf
Fatima Ali <fofoalmali@gmail.com>
2023-08-31 23:25:12 +0300

    feat: add rightSide

---
+++ components/RightSide.tsx
@@ -0,0 +1,12 @@
+const RightSide = () => {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-end gap-6 text-textLight">
+        <a href="malito:ffatima.ali200@gmail.com"> 
+          <p className="text-sm rotate-90 w-72 tracking-wide text-textGreen">ffatima.ali200@gmail.com</p>
+        </a>
+        <span className="w-[2px] h-60 bg-textDark inline-flex"></span>
+    </div>
+  )
+}
+
+export default RightSide
