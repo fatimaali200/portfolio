2c02a25eec8c69d003ebc5c082f41aedfd608c7b
Fatima Ali <fofoalmali@gmail.com>
2023-09-01 18:39:32 +0300

    feat: implement about me section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import LeftSide from "@/components/LeftSide";
 import RightSide from "@/components/RightSide";
 import { motion } from "framer-motion";
 import Banner from "@/components/Banner";
+import About from "@/components/About";
 
 export default function Home() {
   return (
@@ -27,15 +28,16 @@ export default function Home() {
           </motion.div>
           <div className="h-[88vh] w-full mx-auto p-4">
             <Banner />
+            <About />
           </div>
-          <motion.div
+          {/**<motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 1.5 }}
             className="hidden xl:inline-flex w-32 h-full fixed right-0 botom-0"
           >
             <RightSide />
-          </motion.div>
+          </motion.div> */}
         </div>
       </main>
     </>
