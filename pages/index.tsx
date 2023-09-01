c850860c77efc8b6979132302a38855f80781755
Fatima Ali <fofoalmali@gmail.com>
2023-09-01 15:34:45 +0300

    feat: implement banner in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,34 +2,42 @@ import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import LeftSide from "@/components/LeftSide";
 import RightSide from "@/components/RightSide";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Fatima Ali</title>
-      <meta name="description" content=""/>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/logo.ico" />
-    </Head>
-    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-scroll">
-      <Navbar />
-      <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
-        <motion.div 
-        initial={{opacity:0}} 
-        animate={{opacity:1}} 
-        transition={{delay:1.5}} 
-        className="hidden xl:inline-flex w-32 h-full fixed left-0 botom-0"> 
-        <LeftSide />
-        </motion.div>
-        <div className="h-[88vh] mx-auto p-4">Middle</div>
-        <motion.div 
-        initial={{opacity:0}} 
-        animate={{opacity:1}} 
-        transition={{delay:1.5}}  className="hidden xl:inline-flex w-32 h-full fixed right-0 botom-0"><RightSide/></motion.div>
-      </div>
-    </main>
+      <Head>
+        <title>Fatima Ali</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.ico" />
+      </Head>
+      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-scroll">
+        <Navbar />
+        <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full fixed left-0 botom-0"
+          >
+            <LeftSide />
+          </motion.div>
+          <div className="h-[88vh] w-full mx-auto p-4">
+            <Banner />
+          </div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+            className="hidden xl:inline-flex w-32 h-full fixed right-0 botom-0"
+          >
+            <RightSide />
+          </motion.div>
+        </div>
+      </main>
     </>
-    )
+  );
 }
