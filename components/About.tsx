afa43b8c26389dd1b8c6ca36f3047fec04bf66a5
Fatima Ali <fofoalmali@gmail.com>
2023-09-01 18:39:03 +0300

    feat: add about section

---
+++ components/About.tsx
@@ -0,0 +1,100 @@
+import SectionTitle from "./SectionTitle";
+import { AiFillThunderbolt } from "react-icons/ai";
+import Image from "next/image";
+import { profileImg } from "@/public/assets";
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="max-w-containerSmall mx-auto py-10 lgl:py-32 flex flex-col gap-8"
+    >
+      <SectionTitle title="About Me" />
+      <div className="flex flex-col lgl:flex-row gap-16">
+        <div className="w-full lgl:w-2/3 text-base text-textDark font-medium flex flex-col gap-4">
+          <p>
+            Hello, I am Fatima, a Junior Backend Developer with a passion for
+            tackling challenges and collaborating with others to create
+            innovative solutions. I graduated from the Computer Engineering
+            department with a strong foundation in software development and a
+            curiosity to explore the ever-evolving world of technology. My
+            journey in the world of software development has been driven by a
+            relentless desire to learn and grow. I thrive on the challenges that
+            coding presents and find great satisfaction in solving complex
+            problems. Whether it is designing efficient backend systems or
+            optimizing code for performance, I am always up for the task.
+          </p>
+          <p>
+            I have hands-on experience with a variety of technologies,
+            including:
+          </p>
+          <ul className="max-w-[450px] text-sm font-titleFont grid grid-cols-2 gap-2 mt-6">
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              JavaScript
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              TypeScript
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              C#
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              Node.js
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              Express.js
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              Ado.Net
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              MongoDB
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              MSSQL
+            </li>
+          </ul>
+        </div>
+        <div className="w-full lgl:w-1/3 h-80 relative group">
+          <div className="absolute w-full h-80 -left-6 -top-6 rounded-lg">
+            <div className="w-full h-full relative z-20 flex pl-6 lgl:pl-0">
+              <Image
+                className="rounded-lg h-full object-cover"
+                src={profileImg}
+                alt="profileImg"
+              />
+              <div className="hidden lgl:inline-block absolute w-full h-80 bg-textGreen/20 rounded-md top-0 left-0 group-hover:bg-transparent duration-300"></div>
+            </div>
+          </div>
+          <div className="hidden lgl:inline-flex w-full h-80 border-2 border-textGreen rounded-md group-hover:-translate-x-2 group-hover:-translate-y-2 transition-transform duration-300"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
