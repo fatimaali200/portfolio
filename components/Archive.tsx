c432febea410dc3385a472caec75b19d041375d9
Fatima Ali <fofoalmali@gmail.com>
2023-09-05 20:15:49 +0300

    feat:update archive section to be project section

--- components/Archive.tsx
+++ components/Archive.tsx
@@ -1,29 +1,42 @@
 import ArchiveCard from "./ArchiveCard";
+import SectionTitle from "./SectionTitle";
 
+// here if u add a project section just remove section name and id and change it to div
 const Archive = () => {
   return (
-    <div className="max-w-contentContainer mx-auto px-4 py-24">
+    <section id="project" className="max-w-contentContainer mx-auto px-4 py-24">
+      <SectionTitle title="Some Things I have Built" />
       <div className="w-full flex flex-col items-center">
-        <h2 className="text-3xl font-titleFont font-semibold">
-          Other Noteworthy Projects
-        </h2>
-        <p className="text-sm font-titleFont text-textGreen">
-          view the archive
-        </p>
+        {/**<h2 className="text-3xl font-titleFont font-semibold">
+          Other Projects
+        </h2> */}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-10 lgl:px-10">
         <ArchiveCard
           title="Clean Blog Website"
-          des=" This is a simple blog website developed using Node.js, Express.js,
-            and MongoDB. It allows users to add, update, and read blog posts.
-            The project uses the EJS templating engine for rendering views.
-          "
+          des="This is a simple blog website developed using Node.js, Express.js,and MongoDB. It allows users to add, update, and read blog posts.The project uses the EJS templating engine for rendering views."
           listItem={["Nodejs", "MongoDB", "Expressjs", "Ejs-Template-Engine"]}
-          link=""
+          link=" "
           repoLink="https://github.com/fatimaali200/clean-blog-project"
         />
+        <ArchiveCard
+          title="HomeTaste RestApi"
+          des="HomeTaste is a platform that brings together busy individuals, such as students, with skilled local cooks who offer a diverse selection of delicious homemade dishes."
+          listItem={[
+            "Nodejs",
+            "MongoDB",
+            "Expressjs",
+            "Swagger",
+            "JWT",
+            "Passport",
+            "Supertest",
+            "Cloudinray",
+          ]}
+          link="https://home-taste-api.onrender.com/api-docs/"
+          repoLink="https://github.com/fatimaali200/HomeTaste"
+        />
       </div>
-    </div>
+    </section>
   );
 };
 
