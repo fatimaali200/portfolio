148466a6f8dea971c3aa4d920d004fad6c9a3446
Fatima Ali <fofoalmali@gmail.com>
2023-09-05 20:16:37 +0300

    feat: update archivecard function

--- components/ArchiveCard.tsx
+++ components/ArchiveCard.tsx
@@ -1,6 +1,5 @@
 import { FaRegFolder } from "react-icons/fa";
 import { RxOpenInNewWindow } from "react-icons/rx";
-
 interface ProjectInfo {
   title: string;
   des: string;
@@ -8,31 +7,30 @@ interface ProjectInfo {
   listItem: string[];
   repoLink: string;
 }
+
 const ArchiveCard = ({ title, des, listItem, link, repoLink }: ProjectInfo) => {
   return (
-    <a href={link} target="_blank">
-      <div className="w-full h-80 rounded-lg bg-[#112240] p-7 flex flex-col justify-center gap-6 hover:-translate-y-2 transition-transform duration-300 group">
-        <div className="flex justify-between items-center">
-          <a href={repoLink} target="_blank">
-            <FaRegFolder className="text-4xl text-textGreen" />
-          </a>
-          <a href={link} target="_blank">
-            <RxOpenInNewWindow className="text-2xl hover:text-textGreen" />
-          </a>
-        </div>
-        <div>
-          <h2 className="text-xl font-titleFont font-semibold tracking-wide group-hover:text-textGreen">
-            {title}
-          </h2>
-          <p className="text-sm mt-3">{des} </p>
-        </div>
-        <ul className="text-xs mdl:text-sm text-textDark flex items-center gap-2 justify-between flex-wrap">
-          {listItem.map((item, i) => (
-            <li key={i}>{item}</li>
-          ))}
-        </ul>
+    <div className="w-full h-80 rounded-lg bg-[#112240] p-7 flex flex-col justify-center gap-6 hover:-translate-y-2 transition-transform duration-300 group">
+      <div className="flex justify-between items-center">
+        <a href={repoLink} target="_blank">
+          <FaRegFolder className="text-4xl text-textGreen" />
+        </a>
+        <a href={link} target="_blank">
+          <RxOpenInNewWindow className="text-2xl text-textGreen" />
+        </a>
       </div>
-    </a>
+      <div>
+        <h2 className="text-xl font-titleFont font-semibold tracking-wide group-hover:text-textGreen">
+          {title}
+        </h2>
+        <p className="text-sm mt-3">{des} </p>
+      </div>
+      <ul className="text-xs mdl:text-sm text-textDark flex items-center gap-2 justify-between flex-wrap">
+        {listItem.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
