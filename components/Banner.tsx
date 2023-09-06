654bb1c69198f0b95b7dccf6a0a437689bf89516
Fatima Ali <fofoalmali@gmail.com>
2023-09-06 14:39:44 +0300

    feat: update banner section

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,7 +6,6 @@ const Banner = () => {
       id="home"
       className="max-w-contentContainer mx-auto py-10 mdl:py-24 flex flex-col gap-4 lgl:gap-8 mdl:px-10 xl:px-4"
     >
-      {/** <h3 className="text-lg font-titleFont tracking-wide text-textGreen">Hi, I am</h3>*/}
       <motion.h1
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
@@ -22,13 +21,11 @@ const Banner = () => {
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.7 }}
-        className="text-base md:max-w-[650px] text-textDark font-medium"
+        className="text-base md:max-w-[650px] text-textDark font-medium leading-loose"
       >
-        I am a Junior Backend Developer with a strong proficiency in JavaScript,
-        specializing in crafting robust backend applications using Node.js and
-        Express.js. My expertise extends to the effective utilization of NoSQL
-        databases, with a particular focus on MongoDB, to ensure efficient and
-        optimized data management solutions.
+        Passionate Backend Developer proficient in JavaScript, Node.js, and
+        experienced in both relational (SQL) and non-relational (NoSQL)
+        databases. Committed to continuous growth for effective web solutions.
       </motion.p>
     </section>
   );
