324223429563627dabff92a9e00d02bd483bd7e7
Fatima Ali <fofoalmali@gmail.com>
2023-09-11 15:12:00 +0300

    feat: update cv and contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,9 +4,7 @@ const Contact = () => {
       id="contact"
       className="max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center"
     >
-      <h2 className="font-titleFont text-5xl font-semibold">
-        Let&apos;s Get in Touch
-      </h2>
+      <h2 className="font-titleFont text-5xl font-semibold">Get in Touch</h2>
       <p className="max-w-[600px] text-center text-textDark  ">
         If you&apos;d like to connect or have any inquiries, please don&apos;t
         hesitate to reach out. I&apos;m always eager to collaborate, discuss new
@@ -14,7 +12,7 @@ const Contact = () => {
       </p>
       <a href="mailto:ffatima.ali200@gmail.com">
         <button className="w-40 h-14 border border-textGreen mt-6 font-titleFont text-sm text-textGreen tracking-wider rounded-md hover:bg-hoverColor duration-300">
-          Say Hello
+          Contact Me
         </button>
       </a>
     </section>
