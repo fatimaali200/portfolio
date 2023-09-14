a9cc94bbb44ab34cde78588123083aaf9c8fcf92
Fatima Ali <fofoalmali@gmail.com>
2023-09-14 17:26:46 +0300

    feat:update cv

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,4 +1,5 @@
 import logo from "./images/logo.png";
 import profileImg from "./images/profileImg.png";
 import capstonImg from "./images/capstonImg.png";
-export { logo, profileImg, capstonImg };
+import recodedCert from "./images/RecodedCertificate.png";
+export { logo, profileImg, capstonImg, recodedCert };
